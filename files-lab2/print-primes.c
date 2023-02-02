70526be092306e762d818f8012b015e7ef860de0
William Redgård <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>
2023-02-02 18:11:17 +0100

    update

--- files-lab2/print-primes.c
+++ files-lab2/print-primes.c
@@ -12,7 +12,42 @@
 #define COLUMNS 6
 
 
-void print_primes(int n){
+
+int is_prime(int n)
+{
+
+  for (int i = n - 1; i > 1; i--)
+  { // Iterates through all positive numbers smaller than n
+    if (n % i == 0)
+    {           // Checks if n is dividable by i with no rest
+      return 0; // n was dividable by i, hence n cannot be a prime number
+    }
+  }
+  return 1;
+}
+
+void print_number(int n)
+{
+  int COUNTER = 0; // The amount of primen numbers that have been counted
+
+  for (int i = 2; i <= n; i++)
+  {
+    if (is_prime(i))
+    {
+      COUNTER++;
+      printf("        %d", i);
+    }
+
+    if (COUNTER == COLUMNS)
+    {
+      COUNTER = 0;
+      printf("\n");
+    }
+  }
+}
+
+
+/*void print_primes(int n){
   // Should print out all prime numbers less than 'n'
   // with the following formatting. Note that
   // the number of columns is stated in the define
@@ -29,14 +64,14 @@ void print_primes(int n){
   printf("%10d ", 19);
 
   printf("\n");
-}
+}*/
 
 // 'argc' contains the number of program arguments, and
 // 'argv' is an array of char pointers, where each
 // char pointer points to a null-terminated string.
 int main(int argc, char *argv[]){
   if(argc == 2)
-    print_primes(atoi(argv[1]));
+    print_number(atoi(argv[1]));
   else
     printf("Please state an interger number.\n");
   return 0;
